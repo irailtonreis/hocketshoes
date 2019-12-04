4b35aff6c38dc9d1cc85d93f2a47e9184713763b
irailtonreis <=>
2019-12-04 07:19:23 -0300

    Updates

---
+++ precache-manifest.48627e21ccac1d5a13f07713a6f69de6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e1664ba36b9d92a9cc73b600e52b0b1",
+    "url": "/rocketshoes/index.html"
+  },
+  {
+    "revision": "26334514f97ad5da5874",
+    "url": "/rocketshoes/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "26334514f97ad5da5874",
+    "url": "/rocketshoes/static/js/2.a5eade5f.chunk.js"
+  },
+  {
+    "revision": "6748df6a9100f51a62d9",
+    "url": "/rocketshoes/static/js/main.e9db1060.chunk.js"
+  },
+  {
+    "revision": "3650fc069af475c81b6b",
+    "url": "/rocketshoes/static/js/runtime-main.0b919c24.js"
+  },
+  {
+    "revision": "9c43354ab71725d631f7c2a317b870a8",
+    "url": "/rocketshoes/static/media/background.9c43354a.svg"
+  }
+]);
